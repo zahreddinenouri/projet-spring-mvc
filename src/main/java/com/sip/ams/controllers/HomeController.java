eb8942c541e5bbbbb12d8fa25dbe23cb039bee89
zahreddine <zahreddine.nouri16@gmail.com>
2022-02-11 15:51:18 +0100

    passage data vers vue

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 public class HomeController {
 	@RequestMapping ("/info")
-public String info() {
+public String info(Model model) {
+		String formation ="fullstack";
+		model.addAttribute ("workshop",formation);
 	return "home/info" ;
 	
 }
